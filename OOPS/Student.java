4ff62c30937672d6bffda131c8e28826ea6bbf27
Indhu Bhashini V <indhuvnkt@gmail.com>
2023-06-10 13:15:37 +0530

    Create Student.java

---
+++ OOPS/Student.java
@@ -0,0 +1,10 @@
+package OOPS;
+class Student{
+    int id=5;
+    String name="Rahul";
+    public static void main(String args[]){
+        Student s1=new Student();
+        System.out.println(s1.id);
+        System.out.println(s1.name);
+    }
+}
