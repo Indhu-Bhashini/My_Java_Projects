4da41b3cbfc8848f6ed0bf54b81c3b7591cadc54
Indhu Bhashini V <indhuvnkt@gmail.com>
2023-06-10 20:57:29 +0530

    Create Basicobj5.java

---
+++ OOPS/Basicobj5.java
@@ -0,0 +1,15 @@
+package OOPS;
+
+public class Basicobj5 {
+    
+    String name = "Varsha";
+    int year =2000;
+
+    public static void main(String[] args) {
+        Basicobj5 C1 = new Basicobj5();
+        System.out.print("Person name: "+C1.name);
+        System.out.println();
+        System.out.println("Year of born: "+C1.year);
+    }
+}
+
