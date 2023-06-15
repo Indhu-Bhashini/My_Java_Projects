de9ee4db6c03320f4ac10378d10f22e1c43c2e42
Indhu Bhashini V <indhuvnkt@gmail.com>
2023-06-15 12:32:26 +0530

    Create A14.java

---
+++ Arraylist/A14.java
@@ -0,0 +1,18 @@
+package Arraylist;
+import java.util.*;
+public class A14 {
+                
+   public static void main(String args[]){  
+        
+      ArrayList<String> animals=new ArrayList<String>();  
+      animals.add("Dog");
+      animals.add("Cat");
+      animals.add("Panda");
+      animals.add("Horse");
+      animals.add("Fox");
+      animals.add("Giraffe");
+      animals.clear();
+      System.out.println(animals);
+   }  
+}
+
