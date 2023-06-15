9f8f84d10a02ce6640a48b0ff1a42b3b1e367dfb
Indhu Bhashini V <indhuvnkt@gmail.com>
2023-06-15 10:53:56 +0530

    Create A1.java

---
+++ Arraylist/A1.java
@@ -0,0 +1,15 @@
+package Arraylist;
+import java.util.*;
+public class A1 {
+     
+   public static void main(String args[]){  
+        
+      ArrayList<String> animals=new ArrayList<String>();  
+      animals.add("Dog");
+      animals.add("Cat");
+      animals.add("Panda");
+      animals.add("Horse");
+      System.out.println(animals);
+   }  
+}
+
