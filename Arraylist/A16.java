1437d281f8dcaa17b348362d135e83f650a2e7bc
Indhu Bhashini V <indhuvnkt@gmail.com>
2023-06-15 12:38:27 +0530

    Create A16.java

---
+++ Arraylist/A16.java
@@ -0,0 +1,18 @@
+package Arraylist;
+import java.util.*;
+public class A16 {
+    
+
+   public static void main(String args[]){  
+        
+      ArrayList<String> animals=new ArrayList<String>();  
+      animals.add("Dog");
+      animals.add("Cat");
+      animals.add("Panda");
+      animals.add("Horse");
+      animals.add("Fox");
+      animals.add("Giraffe");
+      System.out.println(animals.indexOf("Giraffe"));
+   }  
+}
+
