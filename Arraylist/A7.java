864bf015ffae146e36505911fdb565b52fa071f2
Indhu Bhashini V <indhuvnkt@gmail.com>
2023-06-15 11:21:30 +0530

    Create A7.java

---
+++ Arraylist/A7.java
@@ -0,0 +1,17 @@
+package Arraylist;
+import java.util.*;
+public class A7 {
+
+   public static void main(String args[]){  
+        
+      ArrayList<String> animals=new ArrayList<String>();  
+      animals.add("Dog");
+      animals.add("Cat");
+      animals.add("Panda");
+      animals.add("Horse");
+      System.out.println(animals);
+      System.out.println(animals.size());
+   }  
+}
+    
+
