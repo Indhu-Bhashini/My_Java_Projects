1cac7e61a4f4923f8b095362606d171f123f9fc7
Indhu Bhashini V <indhuvnkt@gmail.com>
2023-06-15 13:54:54 +0530

    Create A26.java

---
+++ Arraylist/A26.java
@@ -0,0 +1,28 @@
+package Arraylist;
+import java.util.ArrayList;
+
+public class A26 {
+    
+
+  public static void main(String[] args) {
+
+    ArrayList<Integer> numbers = new ArrayList<>();
+
+    
+    numbers.add(3);
+    numbers.add(4);
+    numbers.add(5);
+    numbers.add(6);
+    System.out.println("ArrayList: " + numbers);
+
+    System.out.print("Updated ArrayList: ");
+
+    
+    numbers.forEach((e) -> {
+      e = e * e;
+      System.out.print(e + " ");
+    });
+
+  }
+}
+
