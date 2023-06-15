bd81f58155eba4834802c8ee08a29174ef3868c4
Indhu Bhashini V <indhuvnkt@gmail.com>
2023-06-15 11:40:49 +0530

    Create A10.java

---
+++ Arraylist/A10.java
@@ -0,0 +1,16 @@
+package Arraylist;
+import java.util.*;
+public class A10 {
+   public static void main(String args[]){  
+        
+      ArrayList<String> animals=new ArrayList<String>();  
+      animals.add("Dog");
+      animals.add("Cat");
+      animals.add("Panda");
+      animals.add("Horse");
+      animals.add("Fox");
+      animals.add("Giraffe");
+      System.out.println(animals.contains("Horse"));
+   }  
+}
+
